b3e5a898feab2ddca89ded0afd3e7b0018bc8cb7
alvihossain1 <alvinoorhossain@gmail.com>
2025-05-10 18:04:21 +0600

    update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server"
 export { default } from "next-auth/middleware"
 
 export function middleware(request){
-    const cookies = request.cookies.getAll()
-    console.log('Cookies', cookies)
-    return NextResponse.redirect(new URL('/home', request.url))
+    // const cookies = request.cookies.getAll()
+    // console.log('Cookies', cookies)
+    // return NextResponse.redirect(new URL('/home', request.url))
 }
 
-export const config = { matcher: "/dashboard" }
+export const config = { matcher: ["/dashboard"] }
